8c4bf1e7a5663c9d4c94c1b5b8415baf0edc0021
cgynlai <cgynlai@gmail.com>
2022-05-27 13:39:08 +0800

    authentication done

--- PhotoAppApiUsers/src/main/java/com/cyl/api/users/security/WebSecurity.java
+++ PhotoAppApiUsers/src/main/java/com/cyl/api/users/security/WebSecurity.java
@@ -1,11 +1,17 @@
 package com.cyl.api.users.security;
 
+import javax.servlet.Filter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.cyl.api.users.service.UsersService;
 
 @Configuration
 @EnableWebSecurity
@@ -13,6 +19,16 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	
 	
 	private Environment environment;
+	private UsersService usersService;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	public WebSecurity(Environment environment, UsersService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		
+		this.environment = environment;
+		this.usersService = userService;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
 	
 	/*
 	 * @Autowired public WebSecurity(Environment environment) {
@@ -22,7 +38,22 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
     	http.csrf().disable();
     	 http.headers().frameOptions().disable();
-    	http.authorizeRequests().antMatchers("/users/**").permitAll();
+    	http.authorizeRequests().antMatchers("/users/**")
+    	.permitAll().and().addFilter(getAuthenticationFilter());
     	// http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"));
     }
+
+	
+
+	private AuthenticationFilter getAuthenticationFilter() throws Exception {
+		
+		AuthenticationFilter authenticationFilter = new AuthenticationFilter(usersService, environment, authenticationManager());
+	//	authenticationFilter.setAuthenticationManager(authenticationManager());
+		authenticationFilter.setFilterProcessesUrl(environment.getProperty("login.url.path"));
+		return authenticationFilter;
+	}
+	
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usersService).passwordEncoder(bCryptPasswordEncoder);
+	}
 }
--- PhotoAppApiUsers/src/main/java/com/cyl/api/users/service/UserServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/cyl/api/users/service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package com.cyl.api.users.service;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -36,6 +40,21 @@ public class UserServiceImpl implements UsersService {
 		 
 		 UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
 		return returnValue;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		UserEntity userEntity = usersRepository.findByEmail(username);
+		if(userEntity == null) throw new UsernameNotFoundException(username);
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true,true, true,new ArrayList<>());
+	}
+
+	@Override
+	public UserDto getUserDetailsByEmail(String email) {
+		UserEntity userEntity = usersRepository.findByEmail(email);
+		if(userEntity == null) throw new UsernameNotFoundException(email);
+		return new ModelMapper().map(userEntity, UserDto.class);	
+		
 	}
 
 }
